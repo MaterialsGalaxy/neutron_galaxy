9a4f1d7a27969b306a8ecf193572e09945441a1e
MilanSCD <milan.kumar@stfc.ac.uk>
2024-10-17 12:07:36 +0100

    make instrument/sample refinement tool work locally with manual config

--- tools/dev_tools/gsas2_refinement_sample_instrument_prms/README.md
+++ tools/dev_tools/gsas2_refinement_sample_instrument_prms/README.md
@@ -1,6 +1,6 @@
 # GSASII refinement Sample and Instrument Parameters
 
-gsas2_refinement_sample_instrument_prms is a galaxy tool with scripts written in python used to execute an Reitveld refinement in a GSASII project after changing the refinement settings for sample and isntrument parameters. 
+gsas2_refinement_sample_instrument_prms is a galaxy tool with scripts written in python used to execute an Reitveld refinement in a GSASII project after changing the refinement settings for sample and instrument parameters. 
 
 ## Main Features
 
@@ -14,7 +14,7 @@ gsas2_refinement_sample_instrument_prms is a galaxy tool with scripts written in
     - the refinement lst
 
 ## Prerequisites 
-- The upload_gsas2_refinement tool would ideally be used in the workflow at smoe point before this tool is used
+- The upload_gsas2_refinement tool would ideally be used in the workflow at some point before this tool is used
 
 ## Usage 
 - This tool is intended to be used as an intermediate stage of a GSAS2 refinement workflow, but can be used with suitable file inputs.
@@ -24,3 +24,8 @@ gsas2_refinement_sample_instrument_prms is a galaxy tool with scripts written in
     a refinement lst file
     plots of the refinements
 
+## Development notes
+### multiple phases and histograms
+- will have to add a parser method to input lists for each phase / histogram, or have all the inputs create an input text file read into the script
+- python script works locally and any added refinement parameters remain checked in the output gpx. For sequences of refinements important to remember whats checked.
+- tool also works locally. Need to add the config file functionality .  
--- tools/dev_tools/gsas2_refinement_sample_instrument_prms/run_gsas2_sample_instrument_fit.py
+++ tools/dev_tools/gsas2_refinement_sample_instrument_prms/run_gsas2_sample_instrument_fit.py
@@ -1,6 +1,5 @@
 import os
 import sys
-
 # from typing import Union, Dict, Any
 import numpy as np
 
@@ -13,19 +12,14 @@ in the tool xml commands to get this to work
 # import G2script as G2sc
 sys.path.append("/home/mkscd/miniconda3/envs/GSASII/GSAS-II/GSASII")  # needed to "find" GSAS-II modules
 # sys.path.append('/home/mkscd/miniconda3/envs/GSASII/bin') # needed to "find" GSAS-II modules
-import GSASIIscriptable as G2sc
+import GSASIIscriptable as G2sc  # type: ignore
 
 
 def run_gsas2_fit(
     project_fn,
-    # structure_fn,
-    # gsa_fn,
-    # prm_fn,
+    samp_refs,
+    inst_refs,
     output_stem_fn,
-    # stype,
-    # bank,
-    # xmin,
-    # xmax,
     output_path,
     num_cycles=5,
     # init_vals: Union[None, Dict[str, Any]] = None,
@@ -33,22 +27,14 @@ def run_gsas2_fit(
     """
     Parameters
     ----------
-    structure_fn: str
-        input structure cif filename.
-    gsa_fn: str
-        input gsa filename.
-    prm_fn: str
-        input instrument profile filename.
+    project_fn: str
+        GSASII gpx project filename
+    samp_refs: str
+        sample parameters to refine
+    inst_refs: str
+        isntrument parameters to refine
     output_stem_fn: str
         output stem filename.
-    stype: str
-        scattering type
-    banks: str
-        bank 1-6.
-    xmin: float
-        minimum x value
-    xmax: float
-        maximum x value
     output_path: str
         path to put output files
     num_cycles: int
@@ -83,106 +69,29 @@ def run_gsas2_fit(
 
     # load from input project save to new name and directory
     gpx = G2sc.G2Project(gpxfile=project_fn, newgpx=proj_path)
-
+    gpx.save(proj_path)
     # check if the project got created
     if os.path.exists(proj_path):
         print("created project at path:", proj_path)
     else:
         print("no project created at path", proj_path)
-    """
-    Here the project does get created even though the output prints the second statement
-    something is wrong with the output paths in general
-    """
 
     # add six bank histograms to the project
-    hists = []
+    # hists = []
 
-    """
-    Galaxy changes all the input files into .dat files through the <inputs> in the tool xml
-    fix this by reading them in, and saving the encoded text back to the filetypes that GSAS requires.
-    Then pass the path of the new files to the GSAS functions.
-    """
-    """
-    # convert prm file from dat back to prm
-    with open(prm_fn, 'r') as file:
-        content = file.read()
-        prm_fn_fixed = open("new_param_file.prm", "w")
-        prm_fn_fixed.write(content)
-        prm_path = os.path.abspath("new_param_file.prm")
-        #print(content)
-        prm_fn_fixed.close()
+    cell_i = gpx.phases()[0].get_cell()
 
-    # convert data file back to raw
-    with open(gsa_fn, 'r') as file:
-        content = file.read()
-        gsa_fn_fixed = open("new_pwdr_file.raw", "w")
-        gsa_fn_fixed.write(content)
-        gsa_path = os.path.abspath("new_pwdr_file.raw")
-        #print(content)
-        gsa_fn_fixed.close()
-
-    # convert dat file back to CIF
-    with open(structure_fn, 'r') as file:
-        content = file.read()
-        cif_fn_fixed = open("new_cif_file.cif", "w")
-        cif_fn_fixed.write(content)
-        cif_path = os.path.abspath("new_cif_file.cif")
-        #print(content)
-        cif_fn_fixed.close()
-    """
-    # if stype == "N":
-    #    print("heee!!!")
-    # debugging print statements
-    #    print(prm_path)
-    #   print(gsa_path)
-    #    print(cif_path)
-    #    print(bank)
-    # prmFile = "pdfitc/utils/NOMAD_2019B_Si_sixbanks_Shifter_instrument_file.prm"
-    #    hist1 = gpx.add_powder_histogram("new_pwdr_file.raw","new_param_file.prm", databank=bank, instbank=bank)
-    #    print("now!")
-    #    hist1.set_refinements({"Limits": [xmin, xmax]})
-    # if stype == "X":
-    #    print("here! x-ray!!")
-    # prmFile = "pdfitc/utils/PDFNSLSII.instprm"
-    #    hist1 = gpx.add_powder_histogram("new_pwdr_file.raw", "new_param_file.prm")
-    #    hist1.set_refinements({"Limits": [xmin, xmax]})
-
-    # hists.append(hist1)
-
-    # step 2: add a phase and link it to the previous histograms
-    # _ = gpx.add_phase("new_cif_file.cif", phasename="structure", fmthint="CIF", histograms=hists)
-    # print("phase loaded")
-    #cell_i = gpx.phase("structure").get_cell()
-
-    # step 3: increase # of cycles to improve convergence
+    # step 1: increase # of cycles to improve convergence
     gpx.data["Controls"]["data"]["max cyc"] = num_cycles
 
-    # step 4: start refinement
-    # refinement step 1: turn on  Histogram Scale factor
-    refdict1 = {
-        "set": {"Sample Parameters": ["Scale"]},
-        "call": HistStats,
-    }
-    # refinement step 2: turn on background refinement (Hist)
-    """    if init_vals and "bkg" in init_vals:
-        bkg_type = init_vals["bkg"]["Type"]
-        num_coeffs = init_vals["bkg"]["NumCoeffs"]
-        coeffs = init_vals["bkg"]["Coeffs"]
-        refdict2 = {
-            "set": {"Background": {"type": bkg_type, "no. coeffs": num_coeffs, "coeffs": coeffs, "refine": True}},
-            "call": HistStats,
-        }
-    else:
-        refdict2 = {
-            "set": {"Background": {"type": "chebyschev", "no. coeffs": 6, "refine": True}},
-            "call": HistStats,
-        }
-    """
-    # set initial values 
-    #for h in gpx.histograms():
+    # set initial values
+    # for h in gpx.histograms():
     #    h.setHistEntryValue(['Sample Parameters', 'DisplaceX'], 0.001)
     # can also use h.getHistEntryList(keyname='Sample Parameters') to get a list of the values
-    # instrument and sample refinement steps
+
+    #  instrument and sample refinement steps by default will apply to all phases and histograms
+    samp_ref_list = samp_refs.split(',')
+    """
     samplerefdict = {
         "set": {
             "Sample Parameters": [
@@ -193,9 +102,19 @@ def run_gsas2_fit(
             ]
         }
     }
-
-    # instrument refinement steps 
-
+    """
+    samp_ref_dict = {
+        "set": {"Sample Parameters": samp_ref_list},
+        "call": HistStats,
+    }
+    # instrument refinement steps by default will apply to all phases and histograms
+    inst_ref_list = inst_refs.split(',')
+    inst_ref_dict = {
+        "set": {"Instrument Parameters": inst_ref_list},
+        "call": HistStats,
+    }
+    dictList = [samp_ref_dict, inst_ref_dict]
+    """
     instrumentrefdict = {
         "set": {
             "Instrument Parameters":[
@@ -225,17 +144,7 @@ def run_gsas2_fit(
         }
 
     }
-
-
-    # refinement step 3: refine lattice parameter and Uiso refinement (Phase)
-    """    refdict3 = {
-        "set": {"Atoms": {"all": "U"}, "Cell": True},  # set the Uiso and lattice parameters to be refined
-        "call": HistStats,
-    }
-
-    dictList = [refdict1, refdict2, refdict3]
     """
-
     # before fit, save project file first. Then in the future, the refined project file will update this one.
     gpx.save(os.path.join(os.getcwd(), "portal/", output_stem_fn + "_refined.gpx"))
 
@@ -252,12 +161,12 @@ def run_gsas2_fit(
     bkg = np.array(gpx.histogram(0).getdata("Background"))
 
     refs = gpx.histogram(0).reflections()
-    ref_list = refs["structure"]["RefList"]
+    ref_list = refs[gpx.phases()[0].name]["RefList"]
 
     # output_cif_fn = os.path.join(os.getcwd(), 'data/bragg_gsasii/', output_stem_fn + "_refined.cif")
     output_cif_fn = os.path.join(os.getcwd(), "portal/", output_stem_fn + "_refined.cif")
-    gpx.phase("structure").export_CIF(output_cif_fn)
-    cell_r = gpx.phase("structure").get_cell()
+    gpx.phases()[0].export_CIF(output_cif_fn)
+    cell_r = gpx.phases()[0].get_cell()
 
     # header = "Rw = {} \nx           ycalc           y           dy           bkg".format(rw)
     # np.savetxt(f"{output_stem_fn}bank{str(bank)}.dat",
