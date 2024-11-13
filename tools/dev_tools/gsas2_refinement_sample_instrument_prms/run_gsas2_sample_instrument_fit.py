463ade9269eb431166f1d6d09158404341597dcc
MilanSCD <milan.kumar@stfc.ac.uk>
2024-11-13 17:22:48 +0000

    make the sample instrument refinement tool work

--- tools/dev_tools/gsas2_refinement_sample_instrument_prms/run_gsas2_sample_instrument_fit.py
+++ tools/dev_tools/gsas2_refinement_sample_instrument_prms/run_gsas2_sample_instrument_fit.py
@@ -19,6 +19,8 @@ def run_gsas2_fit(
     project_fn,
     samp_refs,
     inst_refs,
+    inst_vals,
+    samp_vals,
     output_stem_fn,
     output_path,
     num_cycles=5,
@@ -33,6 +35,10 @@ def run_gsas2_fit(
         sample parameters to refine
     inst_refs: str
         isntrument parameters to refine
+    inst_vals: list [float]
+        instrument parameter values to set
+    samp_vals: list [float]
+        sample parameter values to set
     output_stem_fn: str
         output stem filename.
     output_path: str
@@ -88,6 +94,62 @@ def run_gsas2_fit(
     # for h in gpx.histograms():
     #    h.setHistEntryValue(['Sample Parameters', 'DisplaceX'], 0.001)
     # can also use h.getHistEntryList(keyname='Sample Parameters') to get a list of the values
+
+    # set instrument and sample values 
+    # get the histogram (for a single powder data file the id is 0)
+    h = gpx.histograms()[0]
+
+    # get the sample parameters we want to change and their values
+    sampleparams = {
+        "Scale": h.getHistEntryValue(['Sample Parameters', 'Scale']),
+        "DisplaceX": h.getHistEntryValue(['Sample Parameters', 'DisplaceX']),
+        "DisplaceY": h.getHistEntryValue(['Sample Parameters', 'DisplaceY']),
+        "Absorption": h.getHistEntryValue(['Sample Parameters', 'Absorption']),
+    }
+    print(samp_vals, "\n", inst_vals, "\n")
+    # set the values in a dictionary
+    i = 0
+    for param in sampleparams:
+        if samp_vals[i] != 0.0:
+            sampleparams[param][0] = samp_vals[i]
+        i += 1
+
+    print(h.getHistEntryValue(['Sample Parameters']), "\n")
+    # set the sample parameters in the project file.
+    for param in sampleparams:
+        h.setHistEntryValue(['Sample Parameters', param], sampleparams[param])
+    print(h.getHistEntryValue(['Sample Parameters']), "\n")
+
+    # get the instrument parameters dictionary
+    instdict = h.getHistEntryValue(['Instrument Parameters'])[0]
+
+    instparams = {
+        "Lam": instdict['Lam'],
+        "Zero": instdict['Zero'],
+        "U": instdict['U'],
+        "V": instdict['V'],
+        "W": instdict['W'],
+        "X": instdict['X'],
+        "Y": instdict['Y'],
+        "Z": instdict['Z'],
+        # "SH/L": instdict['SH/L'],
+    }
+    i = 0
+    for param in instparams:
+        if inst_vals[i] != 0.0:
+            instparams[param][1] = inst_vals[i]
+        i += 1
+
+    print(h.getHistEntryValue(['Instrument Parameters'])[0], "\n")
+    # set the instrument parameters in the project file
+    instdictfull = h.getHistEntryValue(['Instrument Parameters'])
+    for param in instparams:
+        instdictfull[0][param] = instparams[param]
+    h.setHistEntryValue(['Instrument Parameters'],
+                        instdictfull)
+
+    print(h.getHistEntryValue(['Instrument Parameters'])[0], "\n")
+    
 
     #  instrument and sample refinement steps by default will apply to all phases and histograms
     samp_ref_list = samp_refs.split(',')
