12c70e59b403d44d9a77559ac53a3fc6d2ce6535
MilanSCD <milan.kumar@stfc.ac.uk>
2024-11-18 14:43:03 +0000

    configure tools to use GSASII conda environment for openstack server

--- tools/dev_tools/gsas2_refinement_sample_instrument_prms/run_gsas2_sample_instrument_fit.py
+++ tools/dev_tools/gsas2_refinement_sample_instrument_prms/run_gsas2_sample_instrument_fit.py
@@ -10,7 +10,7 @@ locally i added:
 in the tool xml commands to get this to work
 """
 # import G2script as G2sc
-sys.path.append("/home/mkscd/miniconda3/envs/GSASII/GSAS-II/GSASII")
+sys.path.append("/home/dxp41838/miniconda3/envs/GSASII/GSAS-II/GSASII")
 # needed to "find" GSAS-II modules
 import GSASIIscriptable as G2sc  # type: ignore
 
