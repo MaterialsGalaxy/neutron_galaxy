804392d5bdf5f415854dff0739c06cf44706b42b
MilanSCD <milan.kumar@stfc.ac.uk>
2025-01-10 17:34:09 +0000

    update upload tool for multiple file inputs

--- tools/dev_tools/upload_gsas2_refinement/run_gsas2_refinement_upload.py
+++ tools/dev_tools/upload_gsas2_refinement/run_gsas2_refinement_upload.py
@@ -22,11 +22,10 @@ import GSASIIscriptable as G2sc  # type: ignore
 
 
 def run_gsas2_fit(
-    structure_fn,
-    gsa_fn,
-    prm_fn,
+    structure_fns,
+    gsa_fns,
+    prm_fns,
     output_stem_fn,
-    stype,
     output_path,
     num_cycles=5,
     init_vals: Union[None, Dict[str, Any]] = None,
@@ -34,11 +33,11 @@ def run_gsas2_fit(
     """
     Parameters
     ----------
-    structure_fn: str
+    structure_fns:list [str]
         input structure cif filename.
-    gsa_fn: str
+    gsa_fns: list[str]
         input gsa filename.
-    prm_fn: str
+    prm_fns: list [str]
         input instrument profile filename.
     output_stem_fn: str
         output stem filename.
@@ -87,27 +86,19 @@ def run_gsas2_fit(
         print("created project at path:", proj_path)
     else:
         print("no project created at path", proj_path)
+    # add histograms to project
+    for i , gsa_fn in enumerate(gsa_fns):
+        gpx.add_powder_histogram(gsa_fn, prm_fns[i])
 
-    # add histograms to the project
-    hists = []
-    if stype == "N":
-        print("heee!!!")
-        # debugging print statements
-        hist1 = gpx.add_powder_histogram(gsa_fn, prm_fn)
-        print("now!")
-    if stype == "X":
-        print("here! x-ray!!")
-        # prmFile = "pdfitc/utils/PDFNSLSII.instprm"
-        hist1 = gpx.add_powder_histogram(gsa_fn, prm_fn)
 
-    hists.append(hist1)
-
-    # step 2: add a phase and link it to the previous histograms
-    _ = gpx.add_phase(
-        structure_fn, phasename="structure", fmthint="CIF", histograms=hists
-    )
+    # step 2: add phases and link it to the previous histograms
+    for structure_fn in structure_fns:
+        phase_name = os.path.splitext(os.path.basename(structure_fn))[0]
+        gpx.add_phase(
+            structure_fn, phasename=phase_name, fmthint="CIF", histograms=gpx.histograms()
+        )
     print("phase loaded")
-    cell_i = gpx.phase("structure").get_cell()
+
 
     # step 3: increase # of cycles to improve convergence
     gpx.data["Controls"]["data"]["max cyc"] = num_cycles
@@ -156,6 +147,8 @@ def run_gsas2_fit(
     gpx.do_refinements(dictList)
     print("================")
 
+    # save necessary output files to the portal/ directory for galaxy data collection
+
     gpx_output_file_path = os.path.join(os.getcwd(),"portal/", output_stem_fn + "_refined.gpx")
     gpx.save(filename=gpx_output_file_path)
 
@@ -172,13 +165,10 @@ def run_gsas2_fit(
     bkg = np.array(gpx.histogram(0).getdata("Background"))
 
     refs = gpx.histogram(0).reflections()
-    ref_list = refs["structure"]["RefList"]
 
     """output_cif_fn = os.path.join(
         os.getcwd(), "portal/", output_stem_fn + "_refined.cif"
     )
     gpx.phase("structure").export_CIF(output_cif_fn)"""
     
-    cell_r = gpx.phase("structure").get_cell()
-
-    return rw, x, y, ycalc, dy, bkg, cell_i, cell_r, ref_list
+    return rw, x, y, ycalc, dy, bkg
