c1880c1a8df56659931f7678c19196ccfe6d3abe
MilanSCD <milan.kumar@stfc.ac.uk>
2025-01-13 10:52:27 +0000

    remove all old unused static tools

--- tools/dev_tools/interactive_refinement/src/viewmodel.py
+++ tools/dev_tools/interactive_refinement/src/viewmodel.py
@@ -874,9 +874,20 @@ def save_delta(file_name: str, history_id: str) -> str:
     """
     og_project_file = "og_" + file_name
     og_gpx = gsas_load_gpx(og_project_file, og_project_file)
+
+    # ensure phase atom names are loaded
+    og_gpx.index_ids()
+    gpx().index_ids()
+
     diff = DeepDiff(og_gpx, gpx(), exclude_paths="filename")
     delta = Delta(diff)
     delta_file_name = "Delta_on_" + history_id
     with open(delta_file_name, "wb") as dump_file:
         delta.dump(dump_file)
     return delta_file_name
+
+def generate_cifs(current_gpx_id: str) -> None:
+    gxhistory.run_generate_cifs(current_gpx_id)
+    id = refresh_latest_history_entry_id()
+    gxhistory.wait_for_dataset(id)
+    update_history()
